51b1a880aee55e20576107482abf7472ccf2b38b
kannan-ravi <kannanravindran00kr@gmail.com>
2025-04-02 14:36:53 +0530

    react router, tailwind css, login page design completed
    Tailwind css configuration completed, React Router has been added, Login Page Design has completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
