aad41a74452f279bb04d77beabde540ca675c425
kannan-ravi <kannanravindran00kr@gmail.com>
2025-04-02 23:42:10 +0530

    moved api credentials to .env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAwgXG4AynzR9hzzHNS09qXCZmoXEk_QSY",
-  authDomain: "workflow-management-c5f8e.firebaseapp.com",
-  projectId: "workflow-management-c5f8e",
-  storageBucket: "workflow-management-c5f8e.firebasestorage.app",
-  messagingSenderId: "923736890357",
-  appId: "1:923736890357:web:28a6fe42764cc0bfebdebc",
-  measurementId: "G-CFB4BS0PSG",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
